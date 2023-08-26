9ff7936bc0068af34e83b827c6d2f9265f3c89ac
Guillermo Rodriguez <grod937@gmail.com>
2023-08-25 22:37:59 -0700

    Trying out Harpoon!

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -64,7 +64,7 @@ return {
 
 		telescope.load_extension("fzf")
 		telescope.load_extension("ui-select")
-		-- telescope.load_extension("harpoon")
+		telescope.load_extension("harpoon")
 
 		-- set keymaps
 		local keymap = vim.keymap -- for conciseness
@@ -80,7 +80,6 @@ return {
 		) -- find string in current working directory as you type
 		keymap.set("n", "<C-G>", "<cmd>Telescope live_grep<cr>", { desc = "Find string under cursor in cwd" }) -- find string under cursor in current working directory
 		keymap.set("n", "<C-B>", "<cmd>Telescope buffers<cr>", { desc = "Show open buffers" }) -- list open buffers in current neovim instance
-		-- keymap.set("n", "<C-a>h", "<cmd>Telescope harpoon marks<cr>", { desc = "Show harpoon marks" }) -- show harpoon marks
 		-- keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>", { desc = "Show git commits" }) -- list all git commits (use <cr> to checkout) ["gc" for git commits]
 		-- keymap.set( "n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>", { desc = "Show git commits for current buffer" }) -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
 		-- keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>", { desc = "Show git branches" }) -- list git branches (use <cr> to checkout) ["gb" for git branch]
