387bb635a204cd80f7448a8706410180dc2e9dd8
Guillermo Rodriguez <grod937@gmail.com>
2024-01-02 18:30:16 -0800

    Snippets and nodemon

---
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,12 @@
+-------------------------------------
+-- MarkdownPreview:
+-------------------------------------
+return {
+	"iamcco/markdown-preview.nvim",
+	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	build = "cd app && yarn install",
+	init = function()
+		vim.g.mkdp_filetypes = { "markdown" }
+	end,
+	ft = { "markdown" },
+}
--- .config/nvim/snippets/snap.md
+++
@@ -1,7 +0,0 @@
-# SITENAME
-
-<img src="./public/snapshot.png" style="max-height: 500px; box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;" alt="screenshot of SITENAME">
-
-## Notes
-
-* 
